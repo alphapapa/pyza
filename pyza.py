79f3348431d9daf65f335985a4e41079e272a10d
Adam Porter <adam@alphapapa.net>
2015-02-21 21:50:01 -0600

    Reformat User-Agent string to be shorter

--- pyza.py
+++ pyza.py
@@ -15,10 +15,11 @@ import time
 
 class Songza(object):
     SONGZA_URL_PREFIX = 'https://songza.com'
-    REQUEST_HEADERS = {
-        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Accept": "application/json, text/javascript, */*; q=0.01",
-        "User-Agent": "" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"""}
+    REQUEST_HEADERS = {"Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                       "Accept": "application/json, text/javascript, */*; q=0.01",
+                       "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X" 
+                       + "10_8_3) AppleWebKit/537.36 (KHTML, like Gecko)"
+                       + "Chrome/27.0.1453.93 Safari/537.36"}
 
     @staticmethod
     def request(path, params=None, method='get'):
